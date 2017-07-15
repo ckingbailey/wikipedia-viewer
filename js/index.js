5b16b0f2d933a1240932f62beaa877a26fd594c3
ckingbailey <ckingbailey@gmail.com>
2017-07-14 17:57:11 -0700

    clear old cards when user submits new search
    see issue #1

--- js/index.js
+++ js/index.js
@@ -19,15 +19,16 @@ function sendQry(srterm, num){
            format: 'json' },
     dataType: 'jsonp',
     success: function (json) {
-      console.log(json.query.search);
       if(json.query.search[0] === undefined){
         var tryAgain = document.createElement('p');
         tryAgain.innerHTML = 'No results. Please try another search.';
         document.querySelector('.resultBox').appendChild(tryAgain);
       }
+      if(document.querySelector('.resultBox').firstChild){
+        reset();
+      }
       makeCards(json)
       writeCards(json);
-      console.log(json.query.search);
     },
     error: function(err) {
       alert('there was a problem with the request');
@@ -63,7 +64,10 @@ function writeCards(obj){
 }
 
 function reset(){
-
+  var container = document.querySelector('.resultBox');
+  while(container.firstChild){
+    container.removeChild(container.firstChild);
+  }
 }
 
 /*function genSrch(srterm){
